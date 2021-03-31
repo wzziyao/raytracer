eecd6acf58759093fd74db3378aefe8617fae690
wzziyao <ziyaow36@gmail.com>
2021-03-31 00:44:29 -0400

    diffuse spheres with hemispherical scattering

--- src/basic.cpp
+++ src/basic.cpp
@@ -8,10 +8,27 @@
 #include "camera.h"
 #include "material.h"
 #include "hittable_list.h"
+#include <cmath>
 
 using namespace glm;
 using namespace agl;
 using namespace std;
+
+vec3 random_in_unit_sphere() {
+   while (true) {
+      vec3 p = vec3(random_float(-1,1), random_float(-1,1), random_float(-1,1));
+      if (pow(p[0], 2) + pow(p[1], 2) + pow(p[2], 2) >= 1) continue;
+      return p;
+   }
+}
+
+vec3 random_in_hemisphere(const vec3& normal) {
+    vec3 in_unit_sphere = random_in_unit_sphere();
+    if (dot(in_unit_sphere, normal) > 0.0) // In the same hemisphere as the normal
+        return in_unit_sphere;
+    else
+        return -in_unit_sphere;
+}
 
 color ray_color(const ray& r, const hittable_list& world, int depth)
 {
@@ -21,17 +38,11 @@ color ray_color(const ray& r, const hittable_list& world, int depth)
       return color(0);
    }
 
-   if (world.hit(r, 0.001f, infinity, rec))
-   {
-      ray scattered;
-      color attenuation;
-      if (rec.mat_ptr->scatter(r, rec, attenuation, scattered))
-      {
-         color recurseColor = ray_color(scattered, world, depth - 1);
-         return attenuation * recurseColor;
-      }
-      return attenuation;
+   if (world.hit(r, 0.001f, infinity, rec)) {
+      point3 target = rec.p + random_in_hemisphere(rec.normal);
+      return 0.5f * ray_color(ray(rec.p, target - rec.p), world, depth-1);
    }
+
    vec3 unit_direction = normalize(r.direction());
    auto t = 0.5f * (unit_direction.y + 1.0f);
    return (1.0f - t) * color(1, 1, 1) + t * color(0.5f, 0.7f, 1.0f);
@@ -59,8 +70,8 @@ void ray_trace(ppm_image& image)
    int height = image.height();
    int width = image.width();
    float aspect = width / float(height);
-   int samples_per_pixel = 10; // higher => more anti-aliasing
-   int max_depth = 10; // higher => less shadow acne
+   int samples_per_pixel = 50; // higher => more anti-aliasing
+   int max_depth = 50; // higher => less shadow acne
 
    // World
    shared_ptr<material> gray = make_shared<lambertian>(color(0.5f));
